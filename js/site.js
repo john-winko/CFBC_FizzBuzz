6a95655edd10b2a2057e816e6285d666520f62e7
John Winko <john.winko@gmail.com>
2021-10-13 21:35:16 -0500

    My attempt at fizzbuzz

--- js/site.js
+++ js/site.js
@@ -1,4 +1,72 @@
-// Call Hello World
-function HelloWorld(){
-    alert("hello World");
+// 
+function fizzBuzz() {
+    // get fizz and buzz
+    let fizz = document.getElementById("fizzValue").value;
+    let buzz = document.getElementById("buzzValue").value;
+
+    fizz = parseInt(fizz);
+    buzz = parseInt(buzz);
+
+    // validate input
+    let notValid = !Number.isInteger(fizz) || !Number.isInteger(buzz);
+    if (notValid) {
+        alert("Must be a number");
+        return;
+    }
+
+    // generate array[100] of fizz/buzz
+    let arrFizzBuzz = generateFizzBuzz(fizz, buzz);
+
+    // push results to screen
+    displayFizzBuzz(arrFizzBuzz);
+}
+
+function generateFizzBuzz(fizz, buzz) {
+    let arrFB = [0];
+    for (let index = 1; index <= 100; index++) {
+
+        if (index % fizz == 0) {
+            if (index % buzz == 0) {
+                arrFB.push("FizzBuzz")
+                continue;
+            }
+            arrFB.push("Fizz");
+            continue;
+        } else if (index % buzz == 0) {
+            arrFB.push("Buzz");
+            continue;
+        }
+        arrFB.push(index);
+    }
+    return arrFB;
+}
+
+function displayFizzBuzz(arrFizzBuzz) {
+    let tableBody = document.getElementById("results");
+    let tableHeader = document.getElementById("fbHeader");
+    let fbTemplate = document.getElementById("fbTemplate");
+    tableBody.innerHTML = "";
+    tableBody.appendChild(tableHeader);
+    
+    for (let i = 1; i < arrFizzBuzz.length; i += 5) {
+        const templateRow = document.importNode(fbTemplate, true);
+        rowCols = templateRow.querySelectorAll("td");
+
+        rowCols[0].classList.add(arrFizzBuzz[i]);
+        rowCols[0].textContent = arrFizzBuzz[i];
+        
+        rowCols[1].classList.add(arrFizzBuzz[i+1]);
+        rowCols[1].textContent = arrFizzBuzz[i+1];
+        
+        rowCols[2].classList.add(arrFizzBuzz[i+2]);
+        rowCols[2].textContent = arrFizzBuzz[i+2];
+        
+        rowCols[3].classList.add(arrFizzBuzz[i+3]);
+        rowCols[3].textContent = arrFizzBuzz[i+3];
+        
+        rowCols[4].classList.add(arrFizzBuzz[i+4]);
+        rowCols[4].textContent = arrFizzBuzz[i+4];
+
+        tableBody.appendChild(templateRow);
+    }
 }
